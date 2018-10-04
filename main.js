a88c604d0fc7511df8a82f8445505162a7bba44f
Locky <inox127@hotmail.com>
2018-10-04 11:37:56 +0200

    old

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
-let tab = ['B', 'O', 'N', 'J', 'O', 'U', 'R'];
-let verifTab = ['', '', '', '', '', '', ''];
+let tab = ['B','O','N','J','O','U','R'];
+let verifTab = ['','','','','','',''];
+let countTab = [];
 
 function guessLetter() {
-    while(verifTab !== tab) {
-        let lettre = prompt('Entrez une lettre !' + verifTab);
+    while(verifTab.join() !== tab.join()) {
+        let lettre = prompt(`Entrez une lettre : ${verifTab} Essais :${countTab}`).toUpperCase();
+        countTab+= lettre;
+        
         let i = 0;
         while(i < tab.length) {
             if(lettre === tab[i]) {
                 verifTab[i] = tab[i];
+                //countTab = countTab.filter(e=>e===lettre);
             }
             i++;
         }
+        
     }
     alert('FÉLICITATION !');
 }
+
+/*
+function verifL {
+    if (lettre === )    
+};*/
 
 guessLetter();
 
