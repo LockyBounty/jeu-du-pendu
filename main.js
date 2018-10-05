90bf0fd31a87f9c3185d6414fe20c6a7ae36431f
JadFays <faysaljadron@gmail.com>
2018-10-05 14:53:06 +0200

    1.5

--- main.js
+++ main.js
@@ -3,30 +3,38 @@ let verifTab = ['', '', '', '', '', '', ''];
 let memory = [];
 
 function verifContenu(mot) {
-    let i = 0;
-    while(i < memory.length) {
-        if(mot === memory[i]) {
+    for(i = 0; i < memory.length; i++) {
+        if (mot === memory[i]) {
             break;
         }
-        i++;
     }
     if (mot !== memory[i]) {
-       return memory.splice(i, 0, mot);
+        return memory.splice(i, 0, mot);
     }
 }
 
 function guessLetter() {
-    while(verifTab.join() !== tab.join()) { /*on ajoute .join() pour transformer en string afin que les tableaux soient comparable, sinon ça va renvoyer "false"*/
-        let lettre = prompt(`Entrez une lettre : ${verifTab} ${memory}`).toUpperCase();
+    let echec = 0;
+    while (verifTab.join() !== tab.join()) {
+        let lettre = prompt(`DEVINER LE MOT SECRET : ${verifTab}\nLes lettre(s) devinée(s) sont ${memory}`).toUpperCase();
+        let vrai = 0;
+        let faux = 0;
         verifContenu(lettre);
-        for(i = 0; i < tab.length; i++) {
-            if(lettre === tab[i]) {
+        for (i = 0; i < tab.length; i++) {
+            if (lettre === tab[i]) {
+                alert('BIEN !');
                 verifTab.splice(i, 1, tab[i]);
+                vrai = 1;
+            } else {
+                faux = 2;
             }
         }
+        if(vrai !== 1 && faux === 2) {
+            alert('RATÉE');
+            echec++;
+        }   
     }
-    alert(`FÉLICITATION !`);
+    confirm(`FÉLICITATION ! VOUS AVEZ REUSSI AVEC ${echec} FAUTE(S)`);
 }
 
 guessLetter();
-
